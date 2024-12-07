6270cbb2deb1734f7451012f66de256c5e2bc4d5
ANU7MADHAV <anumadhavan888@gmail.com>
2024-12-07 23:09:01 +0530

    Fix : added .env file

--- http/cmd/api/connectDb.go
+++ http/cmd/api/connectDb.go
@@ -9,10 +9,8 @@ import (
 )
 
 func connectDb(cfg config) (*gorm.DB, error) {
-	fmt.Println("hello")
 
-	dsn := "postgresql://neondb_owner:ecpnP1VJag5S@ep-withered-pine-a5byt7vo.us-east-2.aws.neon.tech/prisma_migrate_shadow_db_5a3bc837-e2c9-485b-be77-0776ef4c0379?sslmode=require"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(cfg.db.dsn), &gorm.Config{})
 
 	if err != nil {
 		return nil, fmt.Errorf("cannot connect to database: %w", err)
@@ -22,8 +20,8 @@ func connectDb(cfg config) (*gorm.DB, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error getting database connection pool: %w", err)
 	}
-	sqlDB.SetMaxOpenConns(cfg.db.maxIdleOpenConns)
-	sqlDB.SetMaxIdleConns(cfg.db.maxIdleOpenConns)
+	sqlDB.SetMaxOpenConns(cfg.db.maxOpenConns)
+	sqlDB.SetMaxIdleConns(cfg.db.maxIdleConns)
 
 	duration, _ := time.ParseDuration(cfg.db.maxIdleTime)
 	sqlDB.SetConnMaxIdleTime(duration)
